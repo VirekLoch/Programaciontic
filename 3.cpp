ad10d1327c4678a6cf9ac44ad02fe356cca1b3f8
VirekLoch <39859315+VirekLoch@users.noreply.github.com>
2019-02-13 12:07:21 +0100

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -2,19 +2,19 @@
 int main (void)
 {
 	char nom[100];
-	int sb , ss , imp , sn;
+	float sb , ss , imp , sn;
 	printf("Nombre del empleado\n");
 	gets(nom);
 	fflush(0);
 	printf("Su sueldo bruto \n");
-	scanf("%i", &sb);
+	scanf("%f", &sb);
 	ss = sb * 0.06;
 	imp = sb * 0.1;
 	sn = sb - imp - ss;
 	printf("Nomina de %s \n",nom);
-	printf("Sueldo bruto %d \n",sb);
-	printf("SS %d \n",ss);
-	printf("Impuestos 10%%  %d \n",imp);
-	printf("Sueldo neto %d \n",sn);
+	printf("Sueldo bruto %f €€\n",sb);
+	printf("SS %f \n",ss);
+	printf("Impuestos 10%%  %f \n",imp);
+	printf("Sueldo neto %f \n",sn);
 		
 }
